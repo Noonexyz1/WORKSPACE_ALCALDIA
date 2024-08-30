be6c97964efaa55e9c44797a0ce972f66d13a24c
Diego Kali <aries-d2github@hotmail.com>
2024-08-30 14:43:33 -0400

    refactor: prototipo: Controladores probados con solicitudes postman

--- prototipo/src/main/java/com/prototipo/infrastructure/rest/controller/OperadorController.java
+++ prototipo/src/main/java/com/prototipo/infrastructure/rest/controller/OperadorController.java
@@ -5,10 +5,7 @@ import com.prototipo.infrastructure.rest.response.SolicitudResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +20,21 @@ public class OperadorController {
 
     }
 
-    @PostMapping(path = {"/verSolicitudes"}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = {"/verSolicitudes"}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<SolicitudResponse>> verSolicitudes() {
         List<SolicitudResponse> listRespon = new ArrayList<>();
         listRespon.add(SolicitudResponse.builder()
                         .id(24L)
                         .nroDeCopias(234L)
                         .build());
+        listRespon.add(SolicitudResponse.builder()
+                .id(24L)
+                .nroDeCopias(234L)
+                .build());
+        listRespon.add(SolicitudResponse.builder()
+                .id(24L)
+                .nroDeCopias(234L)
+                .build());
 
         return new ResponseEntity<>(listRespon, HttpStatus.OK);
     }
