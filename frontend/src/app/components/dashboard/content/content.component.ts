798c42db1a174d565e4b68660bcf149d98d769c2
Diego Kali <aries-d2github@hotmail.com>
2024-09-05 10:07:08 -0400

    feat: frontend: Formulario para ver la solicitud a detalle iniciado

--- frontend/src/app/components/dashboard/content/content.component.ts
+++ frontend/src/app/components/dashboard/content/content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DataTableComponent } from "./data-table/data-table.component";
 import { FormNuevoUsuarioComponent } from "./form-nuevo-usuario/form-nuevo-usuario.component";
+import { InfoSolicitudComponent } from "./info-solicitud/info-solicitud.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [DataTableComponent, FormNuevoUsuarioComponent],
+  imports: [DataTableComponent, FormNuevoUsuarioComponent, InfoSolicitudComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
