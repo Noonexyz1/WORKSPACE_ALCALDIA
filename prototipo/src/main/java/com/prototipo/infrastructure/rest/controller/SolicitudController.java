78b78cacb4b19c6afe303ec65d425371e0694070
root <root@kali.kali>
2024-09-19 15:12:20 -0400

    feat: prototipo: Crossorigin para el controlodor Solicitud agregado

--- prototipo/src/main/java/com/prototipo/infrastructure/rest/controller/SolicitudController.java
+++ prototipo/src/main/java/com/prototipo/infrastructure/rest/controller/SolicitudController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 86400)
 @RestController
 @RequestMapping(path = "/solicitante")
 public class SolicitudController {
