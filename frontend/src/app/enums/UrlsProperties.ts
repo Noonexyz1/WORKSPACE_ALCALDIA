90a7d02a88ab7f46618e787c3e9fd3ba6452cffa
root <root@kali.kali>
2024-12-29 10:37:48 -0400

    refactor: frontend: Cambio de Url para cambio de pass

--- frontend/src/app/enums/UrlsProperties.ts
+++ frontend/src/app/enums/UrlsProperties.ts
@@ -2,4 +2,5 @@ export enum UrlsProperties {
   PATH_LOGIN = 'http://localhost:8081/login',
   PATH_DELETE_USER = 'http://localhost:8081/administrador/eliminarFuncionario/',
   PATH_LIST_USERS = 'http://localhost:8081/administrador/listaDeUsuarios',
+  PATH_CHANCE_PASS = 'http://localhost:8081/cambioPassService/cambiarPass'
 }
