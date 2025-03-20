4e2118e042a61dfac6b6487fb6a0b1e5a567c910
root <root@kali.kali>
2025-03-20 11:39:28 -0400

    refactor: breackpoint 51 comp registrar retiro logica de colas terminada con validacion

--- frontend/src/app/components/shared/registrar-retiro/registrar-retiro.component.ts
+++ frontend/src/app/components/shared/registrar-retiro/registrar-retiro.component.ts
@@ -11,18 +11,47 @@ export class RegistrarRetiroComponent {
 
   //A partir del ID de solicitud que me pasen, debo mostrar toda la informacion haciendo peticiones
 
+  listaDeDocumentos: number[] = new Array(3);
+
   arreglo: number[] = [];
 
   constructor() {
-    this.arreglo.push(1);
+    this.listaDeDocumentos.push(1);
+    this.listaDeDocumentos.push(2);
+    this.listaDeDocumentos.push(3);
+
+    //Valor por defecto
+    this.arreglo.push(0);
   }
 
   metodoPrueba() {
     alert("Hola mundo");
   }
 
-  botonPush() {
-    this.arreglo.push(3);
+  inputValue: string = "";
+  selectedValue: string = "";
+  onInputChange(event: Event) {
+    // Obtener el elemento <input> que disparó el evento
+    const inputElement = event.target as HTMLInputElement;
+    const selectElement = document.getElementById('idCategory') as HTMLSelectElement;
+
+    // Obtener el valor del input
+    this.inputValue = inputElement.value;
+    this.selectedValue = selectElement.value;
+  }
+
+  tamDocumentos: number = this.listaDeDocumentos.length - 1;
+  botonPush(event: Event) {
+    if (this.tamDocumentos > 0 && this.selectedValue != "" && this.inputValue != "") {
+      this.onInputChange(event);
+      let tamanoAreglo = this.arreglo.length - 1;
+      this.arreglo[tamanoAreglo] = 1;
+      this.arreglo.push(0);
+      this.tamDocumentos--;
+      // Reiniciar los campos del formulario
+      this.selectedValue = '';
+      this.inputValue = '';
+    }
   }
 
   botonPop() {
