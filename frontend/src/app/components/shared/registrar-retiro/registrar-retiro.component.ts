53332ab835ebbd2ebba197e5ffd07873583cb181
root <root@kali.kali>
2025-03-20 10:16:00 -0400

    refactor: breackpoint 50 formulario refactorizado para evitar el molesto refresh de la pagina

--- frontend/src/app/components/shared/registrar-retiro/registrar-retiro.component.ts
+++ frontend/src/app/components/shared/registrar-retiro/registrar-retiro.component.ts
@@ -9,8 +9,27 @@ import { Component } from '@angular/core';
 })
 export class RegistrarRetiroComponent {
 
+  //A partir del ID de solicitud que me pasen, debo mostrar toda la informacion haciendo peticiones
+
+  arreglo: number[] = [];
+
+  constructor() {
+    this.arreglo.push(1);
+  }
+
   metodoPrueba() {
     alert("Hola mundo");
   }
 
+  botonPush() {
+    this.arreglo.push(3);
+  }
+
+  botonPop() {
+    this.arreglo.pop();
+  }
+
+  botonNotaDePedido() {
+    alert("Nota de Pedido");
+  }
 }
